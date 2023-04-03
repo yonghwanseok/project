16754200009449d445e3fd72aebedd25817f3ae4
HwanSeok Yong <101648597+yonghwanseok@users.noreply.github.com>
2023-04-04 00:47:20 +0900

    footer 컴포넌트

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../../App";
+import './Footer.css'
+
+
+function Footer(props) {
+  return (
+    <footer>
+      <footer className='footerBox'>
+        <h1 class ="footertext"> Maintainview:   </h1>
+        <h1><span class ="footertext"> 권혁빈, 신준철, 용환석, 이기훈  </span></h1>
+    </footer>
+    </footer>
+    
+  );
+}
+
+export default Footer
