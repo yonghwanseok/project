37246a8cfad2b5cf888c5c2ebf6c46a845eb118c
HwanSeok Yong <101648597+yonghwanseok@users.noreply.github.com>
2023-04-04 00:48:52 +0900

    Header컴포넌트

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App";
+import './Header.css';
+import Nav from "../Nav/Nav";
+
+
+const Header = () => {
+  return (
+    <header class="text">
+        <Nav/>
+    </header>
+  );
+};
+
+export default Header;
